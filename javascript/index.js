be90a2b75cf9e71735d9cc9ae260c603677d6aa7
PedroCProgrammer <pedrootaviocouto214@gmail.com>
2021-02-12 14:58:13 -0300

    V7

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,5 @@
-function functionMenu(){
-    console.log('clicou!')
+function nana(){
+    let tittle = document.querySelector('h1#pizza-sabor-type')
+    console.log(tittle)
+    tittle.innerText = 'teste1'
 }
